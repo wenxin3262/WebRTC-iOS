c90532228840566b80035d1a3230b05af44f030d
Xin Wen <wenxin3262@gmail.com>
2017-01-01 22:40:43 -0800

    add test scheme

---
+++ WebRTCDemo/WebRTCDemoTests/WebRTCDemoTests.swift
@@ -0,0 +1,35 @@
+//
+//  WebRTCDemoTests.swift
+//  WebRTCDemoTests
+//
+//  Created by Xin Wen on 1/1/17.
+//  Copyright © 2017 WebRTCDemo. All rights reserved.
+//
+
+import XCTest
+
+class WebRTCDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
